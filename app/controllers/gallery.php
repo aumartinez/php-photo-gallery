da07dbdb7f29309f50347b10dbebda9cffaa51c4
Augusto Martinez <acm_asd@hotmail.com>
2020-04-28 16:53:01 -0600

    Update gallery.php

--- app/controllers/gallery.php
+++ app/controllers/gallery.php
@@ -1,98 +1,157 @@
 <?php
 
-class Gallery extends Controller {
+class UploadModel extends DbModel {
+  protected $sanitized = array();
+  protected $user;
   
-  protected $output;  
-    
-  public function __construct($controller, $method) {
-    parent::__construct($controller, $method);
-    
-    session_start();
-        
-    # Any models required to interact with this controller should be loaded here    
-    $this->load_model("DbModel");
-    $this->load_model("PageModel");
-    $this->load_model("StartupModel");
-    
-    # Instantiate custom view output
-    $this->output = new PageView();
-    
-    # Startup methods
-    $this->startup();
+  # Allowed types
+  protected $allowed = array("jpg", "png", "jpeg", "gif");
+  # Max size (2MB)
+  protected $max_size = 2 * 1024 * 1024;
+  # Max image upload
+  protected $limit = 3;
+  
+  # Upload required
+  public function required() {
+    if (!isset($_POST["user"]) || $_POST["user"] == "") {
+      $_SESSION["error"][] = "Invalid entry";
+    }    
+    $this->error_check("gallery");
   }
   
-  # Each method will request the model to present the local resource
-  public function index() {        
+  # Sanitize user input
+  public function sanitize_post() {
+    $this->sanitized = array();
     
-    $active_key = "ACTIVE";
-    $active_class = "";
+    foreach ($_POST as $key => $value) {
+      $this->sanitized[$key] = $this->open_link()->real_escape_string($value);
+    }
     
-    $this->output->add_locale($active_key, $active_class);
+    return $this->sanitized;
+  }
+  
+  # Empty submit check
+  public function empty_submit() {
+    # Empty array check    
+    if (empty(array_filter($_FILES["images"]["name"]))) {
+      $_SESSION["error"][] = "No images selected";
+    }
     
-    $err_key = "UPLOAD_ERROR";
-    $err_mess = "";
-    
-    if (isset($_SESSION["error"]) && isset($_SESSION["submit-form"])){
-      $active_key = "ACTIVE";
-      $active_class = "active";
+    $this->error_check("gallery");
+  }
+  
+  # Max image size
+  public function max_images() {
+    if (count($_FILES["images"]["name"]) > $this->limit) {
+      $_SESSION["error"][] = "Please upload only " . $this->limit . " images";
+      $this->error_check("gallery");
+    }
+  }
+  
+  # File type check
+  public function type_check() {
+    foreach ($_FILES["images"]["tmp_name"] as $key => $value) {
+      $file_name = $_FILES["images"]["name"][$key];
+      $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
       
-      $this->output->add_locale($active_key, $active_class);
-      
-      $err_mess = "\n";
-      $err_mess .= "Errors found!";
-      $err_mess .= "<br />\n";
-      
-      foreach ($_SESSION["error"] as $error) {
-        $err_mess .= $error . "<br />\n";
+      if (!in_array($file_ext, $this->allowed)) {
+        $_SESSION["error"][] = "Invalid image file type: " . $file_ext;
       }
-    
-      unset($_SESSION["error"]);
-      unset($_SESSION["submit-form"]);
     }
     
-    $this->output->add_locale($err_key, $err_mess);  
-    $this->build_page("gallery");  
+    $this->error_check("gallery");
   }
   
-  # Not found handler
-  public function not_found() {
-    # 404 page
-    $this->build_page("not-found");    
-  }
-  
-  # Start setup
-  private function startup() {
-    # If DB doesn't exists create it
-    if (!$this->get_model("DbModel")->test_db()) {
-      $this->get_model("StartupModel")->first_run();
-      $this->redirect("gallery");
+  # Max size check
+  public function size_check() {
+    foreach ($_FILES["images"]["tmp_name"] as $key => $value) {
+      $file_size = $_FILES["images"]["size"][$key];
+      
+      if ($file_size > $this->max_size) {
+        $_SESSION["error"][] = "Images should be less than 2 MB";
+      }
     }
     
-    # If DB tables aren't setup, create them
-    if (!$this->get_model("StartupModel")->test_tables()) {
-      if (file_exists(ROOT . DS . "config" . DS . "createtables.sql")) {
-        $sql = file_get_contents(ROOT . DS . "config" . DS . "createtables.sql");
-        $this->get_model("StartupModel")->setup_tables($sql);
-        $this->redirect("gallery");
+    $this->error_check("gallery");
+  }  
+  
+  # Upload
+  public function upload() {
+    # Target dir
+    $target_dir = UPLOAD;
+    
+    foreach ($_FILES["images"]["tmp_name"] as $key => $value) {
+      $file_tempname = $_FILES["images"]["tmp_name"][$key];
+      $file_name = $_FILES["images"]["name"][$key];
+      $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+      
+      $hash = uniqid("", TRUE);
+      $hash = md5($hash);
+      $hash = $this->open_link()->real_escape_string($hash);
+      $this->close_link();
+      
+      $file_path = $target_dir . $hash . "." . $file_ext;
+      
+      $user = $_POST["user"];
+      $user = $this->open_link()->real_escape_string($user);
+      $this->close_link();
+      
+      $file_caption = $_POST["caption"];
+      $file_caption = $this->open_link()->real_escape_string($file_caption);
+      $this->close_link();
+      
+      $this->user = $user;
+      
+      $full_name = strtok($file_name,".") . "." .$file_ext;
+      $hash_name = $hash . "." . $file_ext;
+      
+      $sql = "SELECT *
+              FROM gallery
+              WHERE file_name = '{$full_name}'";
+              
+      $found = $this->get_rows($sql);
+      
+      if ($found > 0) {
+        $full_name = strtok($file_name,".") . time() . $file_ext;
+      }
+      
+      if (move_uploaded_file($file_tempname, $file_path)) {
+        $sql = "INSERT INTO gallery (
+                user,
+                file_name,
+                file_path,
+                file_caption,
+                created_date
+                )
+                VALUES (
+                '{$this->user}',
+                '{$full_name}',
+                '{$hash_name}',
+                '{$file_caption}',
+                NOW()
+                )";
+        
+        $this->set_query($sql); 
       }
       else {
-        $this->build_page("db-error");
+        $_SESSION["error"][] = "Error uploading " . $full_name;
       }
     }
     
+    $this->error_check("gallery");
   }
   
-  # Controller/Model/View link
-  protected function build_page($page_name) {    
-    $html_src = $this->get_model("PageModel")->get_page($page_name);    
-    $html = $this->output->replace_localizations($html_src);
-    
-    $this->get_view()->render($html);
+  # Error check method
+  protected function error_check($page) {
+    if (count($_SESSION["error"]) > 0) {
+      error_log("Error : " . $this->user);
+      $this->redirect(PATH . "/" . $page);
+    }
   }
   
   # Redirect
-  protected function redirect($page) {
-    header ("Location: /" . PATH . "/" . $page);
+  private function redirect($page) {
+    header ("Location: /" . $page);
     exit();
   }
   
